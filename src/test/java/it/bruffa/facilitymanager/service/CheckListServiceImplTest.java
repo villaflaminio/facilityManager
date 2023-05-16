843d5252aef0d3dad986fb3bd73fae60bbbf9145
villaflaminio <vifla01@gmail.com>
2023-05-16 17:40:00 +0200

    CheckListServiceImplTest

--- src/test/java/it/bruffa/facilitymanager/service/CheckListServiceImplTest.java
+++ src/test/java/it/bruffa/facilitymanager/service/CheckListServiceImplTest.java
@@ -1,133 +1,141 @@
-//package it.bruffa.facilitymanager.service;
-//import static org.junit.Assert.assertEquals;
-//import org.junit.Test;
-//import org.apache.log4j.Logger;
-//import static org.junit.jupiter.api.Assertions.fail;
-//import org.junit.jupiter.api.AfterAll;
-//import org.junit.jupiter.api.AfterEach;
-//import org.junit.jupiter.api.BeforeAll;
-//import org.junit.jupiter.api.BeforeEach;
-//import org.junit.jupiter.api.Disabled;
-//import org.junit.jupiter.api.Test;
-//public class CheckListServiceImplTest {
-//
-//	private Logger log = Logger.getLogger(this.getClass());
-//    @BeforeAll
-//    static void initAll() {
-//    }
-//    @BeforeEach
-//    void init() {
-//    }
-//
-//  @Test
-//	@DisplayName("get Check List By Id")
-//    public void getCheckListById(){
-//		try {
-//			log.info("Starting execution of getCheckListById");
-//ResponseEntity<CheckList> expectedValue = null;
-//  Long checkListId=0;
-//
-//
-// CheckListServiceImpl checklistserviceimpl  =new CheckListServiceImpl();
-//ResponseEntity<CheckList> actualValue=checklistserviceimpl.getCheckListById( checkListId);
-//	  log.info("Expected Value="+ expectedValue +" . Actual Value="+actualValue);
-//	  System.out.println("Expected Value="+ expectedValue +" . Actual Value="+actualValue);
-//Assertions.assertEquals(expectedValue, actualValue);
-//		} catch (Exception exception) {
-//			log.error("Exception in execution of execute1GetAllLogFromFirstMovF-"+exception,exception);
-//			exception.printStackTrace();
-//			Assertions.assertFalse(false);
-//		}
-//    }
-//
-//  @Test
-//	@DisplayName("get Check List By Name")
-//    public void getCheckListByName(){
-//		try {
-//			log.info("Starting execution of getCheckListByName");
-//ResponseEntity<CheckList> expectedValue = null;
-//  String checkListName="";
-//
-//
-// CheckListServiceImpl checklistserviceimpl  =new CheckListServiceImpl();
-//ResponseEntity<CheckList> actualValue=checklistserviceimpl.getCheckListByName( checkListName);
-//	  log.info("Expected Value="+ expectedValue +" . Actual Value="+actualValue);
-//	  System.out.println("Expected Value="+ expectedValue +" . Actual Value="+actualValue);
-//Assertions.assertEquals(expectedValue, actualValue);
-//		} catch (Exception exception) {
-//			log.error("Exception in execution of execute1GetAllLogFromFirstMovF-"+exception,exception);
-//			exception.printStackTrace();
-//			Assertions.assertFalse(false);
-//		}
-//    }
-//
-//  @Test
-//	@DisplayName("create Check List")
-//    public void createCheckList(){
-//		try {
-//			log.info("Starting execution of createCheckList");
-//ResponseEntity<CheckList> expectedValue = null;
-//  CreateCheckListRequest createCheckListRequest = null;
-//
-//
-// CheckListServiceImpl checklistserviceimpl  =new CheckListServiceImpl();
-//ResponseEntity<CheckList> actualValue=checklistserviceimpl.createCheckList( createCheckListRequest);
-//	  log.info("Expected Value="+ expectedValue +" . Actual Value="+actualValue);
-//	  System.out.println("Expected Value="+ expectedValue +" . Actual Value="+actualValue);
-//Assertions.assertEquals(expectedValue, actualValue);
-//		} catch (Exception exception) {
-//			log.error("Exception in execution of execute1GetAllLogFromFirstMovF-"+exception,exception);
-//			exception.printStackTrace();
-//			Assertions.assertFalse(false);
-//		}
-//    }
-//
-//  @Test
-//	@DisplayName("update Check List")
-//    public void updateCheckList(){
-//		try {
-//			log.info("Starting execution of updateCheckList");
-//ResponseEntity<CheckList> expectedValue = null;
-//  Long checkListId=0;
-//CreateCheckListRequest createCheckListRequest = null;
-//
-//
-// CheckListServiceImpl checklistserviceimpl  =new CheckListServiceImpl();
-//ResponseEntity<CheckList> actualValue=checklistserviceimpl.updateCheckList( checkListId ,createCheckListRequest);
-//	  log.info("Expected Value="+ expectedValue +" . Actual Value="+actualValue);
-//	  System.out.println("Expected Value="+ expectedValue +" . Actual Value="+actualValue);
-//Assertions.assertEquals(expectedValue, actualValue);
-//		} catch (Exception exception) {
-//			log.error("Exception in execution of execute1GetAllLogFromFirstMovF-"+exception,exception);
-//			exception.printStackTrace();
-//			Assertions.assertFalse(false);
-//		}
-//    }
-//
-//  @Test
-//	@DisplayName("delete Check List")
-//    public void deleteCheckList(){
-//		try {
-//			log.info("Starting execution of deleteCheckList");
-//ResponseEntity<Boolean> expectedValue = null;
-//  Long checkListId=0;
-//
-//
-// CheckListServiceImpl checklistserviceimpl  =new CheckListServiceImpl();
-//ResponseEntity<Boolean> actualValue=checklistserviceimpl.deleteCheckList( checkListId);
-//	  log.info("Expected Value="+ expectedValue +" . Actual Value="+actualValue);
-//	  System.out.println("Expected Value="+ expectedValue +" . Actual Value="+actualValue);
-//Assertions.assertEquals(expectedValue, actualValue);
-//		} catch (Exception exception) {
-//			log.error("Exception in execution of execute1GetAllLogFromFirstMovF-"+exception,exception);
-//			exception.printStackTrace();
-//			Assertions.assertFalse(false);
-//		}
-//    }
-//    @AfterEach
-//    void tearDown() {
-//    }
-//    @AfterAll
-//    static void tearDownAll() {
-//    }
-//}
+package it.bruffa.facilitymanager.service;
+import it.bruffa.facilitymanager.model.dto.request.CreateCheckListRequest;
+import it.bruffa.facilitymanager.model.dto.request.CreateStructureRequest;
+import it.bruffa.facilitymanager.model.entity.CheckList;
+import it.bruffa.facilitymanager.model.entity.Reservation;
+import it.bruffa.facilitymanager.model.entity.Structure;
+import it.bruffa.facilitymanager.model.projection.StructureIdInfo;
+import it.bruffa.facilitymanager.model.projection.StructureInfo;
+import it.bruffa.facilitymanager.repository.*;
+import it.bruffa.facilitymanager.service.impl.CheckListServiceImpl;
+import it.bruffa.facilitymanager.service.impl.StructureServiceImpl;
+import org.instancio.Instancio;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.projection.ProjectionFactory;
+import org.springframework.data.projection.SpelAwareProxyProjectionFactory;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.*;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+@ExtendWith(MockitoExtension.class)
+public class CheckListServiceImplTest {
+
+    private static final Logger log = LoggerFactory.getLogger(CheckListServiceImplTest.class);
+    @Mock
+    private CheckListRepository checkListRepository;
+    @InjectMocks
+    private CheckListServiceImpl checkListserviceimpl;
+    @Test
+    @DisplayName("get CheckList By Id")
+    public void getCheckListById() {
+        try {
+            log.info("Starting execution of getCheckListById");
+            CheckList checkList = Instancio.create(CheckList.class);
+
+            when(checkListRepository.findById(anyLong())).thenReturn(Optional.of(checkList));
+            Long checkListId = checkList.getId();
+
+            ResponseEntity<CheckList> actualValue = checkListserviceimpl.getCheckListById(checkListId);
+            Assertions.assertEquals(checkList, actualValue.getBody());
+        } catch (Exception exception) {
+            log.error("Exception in execution of execute1GetAllLogFromFirstMovF-" + exception, exception);
+            exception.printStackTrace();
+            Assertions.assertFalse(false);
+        }
+    }
+
+    @Test
+    @DisplayName("get Check List By Name")
+    public void getCheckListByName() {
+        try {
+            log.info("Starting execution of getCheckListByName");
+            log.info("Starting execution of getCheckListById");
+
+            CheckList checkList = Instancio.create(CheckList.class);
+
+            when(checkListRepository.findByName(any())).thenReturn(Optional.of(checkList));
+
+            ResponseEntity<CheckList> actualValue = checkListserviceimpl.getCheckListByName(checkList.getName());
+            Assertions.assertEquals(checkList, actualValue.getBody());
+        } catch (Exception exception) {
+            log.error("Exception in execution of execute1GetAllLogFromFirstMovF-" + exception, exception);
+            exception.printStackTrace();
+            Assertions.assertFalse(false);
+        }
+    }
+
+    @DisplayName("create CheckList")
+    @Test
+    public void createCheckList() {
+        try {
+            log.info("Starting execution of createCheckList");
+            CreateCheckListRequest createCheckListRequest = Instancio.create(CreateCheckListRequest.class);
+
+            ResponseEntity<CheckList> actualValue = checkListserviceimpl.createCheckList(createCheckListRequest);
+
+            Assertions.assertEquals(actualValue.getStatusCode(), HttpStatusCode.valueOf(200));
+        } catch (Exception exception) {
+            log.error("Exception in execution of execute1GetAllLogFromFirstMovF-" + exception, exception);
+            exception.printStackTrace();
+            Assertions.assertFalse(false);
+        }
+    }
+
+    @Test
+    @DisplayName("update CheckList")
+    public void updateCheckList() {
+        try {
+            log.info("Starting execution of updateCheckList");
+            CheckList checkList = Instancio.create(CheckList.class);
+            Long checkListId = checkList.getId();
+
+            when(checkListRepository.findById(anyLong())).thenReturn(Optional.of(checkList));
+            CreateCheckListRequest checkListRequest =  Instancio.create(CreateCheckListRequest.class);
+
+
+            ResponseEntity<CheckList> actualValue = checkListserviceimpl.updateCheckList(checkListId, checkListRequest);
+
+            Assertions.assertEquals(actualValue.getStatusCode(), HttpStatusCode.valueOf(200));
+        } catch (Exception exception) {
+            log.error("Exception in execution of execute1GetAllLogFromFirstMovF-" + exception, exception);
+            exception.printStackTrace();
+            Assertions.assertFalse(false);
+        }
+    }
+    @Test
+    @DisplayName("delete CheckList")
+    public void deleteCheckList() {
+        try {
+            log.info("Starting execution of deleteCheckList");
+            ResponseEntity<Boolean> expectedValue = ResponseEntity.ok(true);
+            CheckList checkList = Instancio.create(CheckList.class);
+            Long checkListId = checkList.getId();
+
+            when(checkListRepository.findById(anyLong())).thenReturn(Optional.of(checkList));
+
+            ResponseEntity<Boolean> actualValue = checkListserviceimpl.deleteCheckList(checkListId);
+            log.info("Expected Value=" + expectedValue + " . Actual Value=" + actualValue);
+            System.out.println("Expected Value=" + expectedValue + " . Actual Value=" + actualValue);
+            Assertions.assertEquals(expectedValue, actualValue);
+        } catch (Exception exception) {
+            log.error("Exception in execution of execute1GetAllLogFromFirstMovF-" + exception, exception);
+            exception.printStackTrace();
+            Assertions.assertFalse(false);
+        }
+    }
+
+
+}
