be5c4fe1ac8c241f480f23a575efaf47d703734e
villaflaminio <vifla01@gmail.com>
2023-05-16 18:56:27 +0200

    end tests

--- src/test/java/it/bruffa/facilitymanager/service/FileServiceImplTest.java
+++ src/test/java/it/bruffa/facilitymanager/service/FileServiceImplTest.java
@@ -1,4 +1,5 @@
 package it.bruffa.facilitymanager.service;
+
 import it.bruffa.facilitymanager.model.dto.request.CreateCheckListRequest;
 import it.bruffa.facilitymanager.model.dto.request.CreateStructureRequest;
 import it.bruffa.facilitymanager.model.dto.request.UpdateQuoteRequest;
@@ -7,10 +8,7 @@ import it.bruffa.facilitymanager.model.projection.AccessLogInfo;
 import it.bruffa.facilitymanager.model.projection.StructureIdInfo;
 import it.bruffa.facilitymanager.model.projection.StructureInfo;
 import it.bruffa.facilitymanager.repository.*;
-import it.bruffa.facilitymanager.service.impl.AccessLogServiceImpl;
-import it.bruffa.facilitymanager.service.impl.CheckListServiceImpl;
-import it.bruffa.facilitymanager.service.impl.QuoteServiceImpl;
-import it.bruffa.facilitymanager.service.impl.StructureServiceImpl;
+import it.bruffa.facilitymanager.service.impl.*;
 import org.instancio.Instancio;
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -41,98 +39,50 @@ import org.springframework.web.multipart.MultipartFile;
 
 @ExtendWith(MockitoExtension.class)
 public class FileServiceImplTest {
-  
-	private Logger log = Logger.getLogger(this.getClass());
-    @BeforeAll
-    static void initAll() {
+
+    private static final Logger log = LoggerFactory.getLogger(QuoteServiceImplTest.class);
+
+    @InjectMocks
+    FileServiceImpl fileserviceimpl;
+    @Mock
+    private FileRepository fileRepository;
+
+    @Test
+    @DisplayName("store")
+    public void store() {
+        try {
+            log.info("Starting execution of store");
+            MultipartFile file = new MockMultipartFile("file.txt", new FileInputStream(new ClassPathResource("/init/file.txt").getFile()));
+
+
+            File actualValue = fileserviceimpl.store(file);
+
+        } catch (Exception exception) {
+            log.error("Exception in execution of execute1GetAllLogFromFirstMovF-" + exception, exception);
+            exception.printStackTrace();
+            Assertions.assertFalse(false);
+        }
     }
-    @BeforeEach
-    void init() {
+
+
+    @Test
+    @DisplayName("get File")
+    public void getFile() {
+        try {
+            log.info("Starting execution of getFile");
+            File expectedValue = Instancio.create(File.class);
+            Long id = expectedValue.getId();
+            when(fileRepository.findById(anyLong())).thenReturn(Optional.of(expectedValue));
+
+            File actualValue = fileserviceimpl.getFile(id);
+            log.info("Expected Value=" + expectedValue + " . Actual Value=" + actualValue);
+            System.out.println("Expected Value=" + expectedValue + " . Actual Value=" + actualValue);
+            Assertions.assertEquals(expectedValue, actualValue);
+        } catch (Exception exception) {
+            log.error("Exception in execution of execute1GetAllLogFromFirstMovF-" + exception, exception);
+            exception.printStackTrace();
+            Assertions.assertFalse(false);
+        }
     }
- 
-  @Test  
-	@DisplayName("store")
-    public void store(){  
-		try {
-			log.info("Starting execution of store");
-File expectedValue = null; 
-  MultipartFile file = null; 
- 
-  
- FileServiceImpl fileserviceimpl  =new FileServiceImpl(); 
-File actualValue=fileserviceimpl.store( file);
-	  log.info("Expected Value="+ expectedValue +" . Actual Value="+actualValue);
-	  System.out.println("Expected Value="+ expectedValue +" . Actual Value="+actualValue);
-Assertions.assertEquals(expectedValue, actualValue);
-		} catch (Exception exception) {
-			log.error("Exception in execution of execute1GetAllLogFromFirstMovF-"+exception,exception);
-			exception.printStackTrace();
-			Assertions.assertFalse(false);
-		}
-    }  
- 
-  @Test  
-	@DisplayName("store Exception")
-    public void storeException(){  
-		try {
-			log.info("Starting execution of storeException");
- MultipartFile file = null; 
- 
-  
- FileServiceImpl fileserviceimpl  =new FileServiceImpl(); 
-fileserviceimpl.storeException( file);
-Assertions.assertTrue(false);
-		} catch (Exception exception) {
-			log.error("Exception in execution ofstoreException-"+exception,exception);
-			exception.printStackTrace();
-			Assertions.assertFalse(true);
-		}
-    }  
- 
-  @Test  
-	@DisplayName("get File")
-    public void getFile(){  
-		try {
-			log.info("Starting execution of getFile");
-File expectedValue = null; 
-  Long id=0; 
- 
-  
- FileServiceImpl fileserviceimpl  =new FileServiceImpl(); 
-File actualValue=fileserviceimpl.getFile( id);
-	  log.info("Expected Value="+ expectedValue +" . Actual Value="+actualValue);
-	  System.out.println("Expected Value="+ expectedValue +" . Actual Value="+actualValue);
-Assertions.assertEquals(expectedValue, actualValue);
-		} catch (Exception exception) {
-			log.error("Exception in execution of execute1GetAllLogFromFirstMovF-"+exception,exception);
-			exception.printStackTrace();
-			Assertions.assertFalse(false);
-		}
-    }  
- 
-  @Test  
-	@DisplayName("get All Files")
-    public void getAllFiles(){  
-		try {
-			log.info("Starting execution of getAllFiles");
-Stream<File> expectedValue = null; 
-   
-  
- FileServiceImpl fileserviceimpl  =new FileServiceImpl(); 
-Stream<File> actualValue=fileserviceimpl.getAllFiles();
-	  log.info("Expected Value="+ expectedValue +" . Actual Value="+actualValue);
-	  System.out.println("Expected Value="+ expectedValue +" . Actual Value="+actualValue);
-Assertions.assertEquals(expectedValue, actualValue);
-		} catch (Exception exception) {
-			log.error("Exception in execution of execute1GetAllLogFromFirstMovF-"+exception,exception);
-			exception.printStackTrace();
-			Assertions.assertFalse(false);
-		}
-    }  
-    @AfterEach
-    void tearDown() {
-    }
-    @AfterAll
-    static void tearDownAll() {
-    }
+
 }
